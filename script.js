41fbea840ea027221a45468cc3d09a841c28d23b
Matheus Ritzmann <123838378+Hiroshi1401@users.noreply.github.com>
2024-05-15 22:07:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function escolhaGuardaChuva() {
+    // Redirecionar para o link do vídeo do Twitter
+    window.location.href = "https://x.com/PostsOfCats/status/1788139661581279427"; 
+}
+
+function escolhaCorrer() {
+    document.getElementById('feedback').innerHTML = "Desculpa, escolha errada, tente novamente :)";
+}
